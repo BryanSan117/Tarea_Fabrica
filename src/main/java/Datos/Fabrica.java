b1a2e81d91134437c21ea3fc46090896a9b0fbcb
BryanSan117 <brsandovalc@alumno.uspg.edu.gt>
2021-08-19 17:22:01 -0500

    Commit2

--- src/main/java/Datos/Fabrica.java
+++ src/main/java/Datos/Fabrica.java
@@ -17,45 +17,34 @@ public class Fabrica {
 		cx1.desconectar();
 		System.out.println("**************************************************");
 	
-		
-		iconexion cx2 = Fabrica.getConexion("Vacia");
+		iconexion cx2 = Fabrica.getConexion("SQL");
 		System.out.println();
-		System.out.println("2.  Conexion vacia no hay ninguna base de datos");
+		System.out.println("3.  Conexion  con SQL Server");
 		System.out.println();
 		System.out.println("**************************************************");
 		cx2.conectar();
 		System.out.println("**************************************************");
 		cx2.desconectar();
 		System.out.println("**************************************************");
-		
-		iconexion cx3 = Fabrica.getConexion("SQL");
+
+		iconexion cx3 = Fabrica.getConexion("MysQL");
 		System.out.println();
-		System.out.println("3.  Conexion  con SQL Server");
+		System.out.println("4.	Conexion  con MySQL Server");
 		System.out.println();
 		System.out.println("**************************************************");
 		cx3.conectar();
 		System.out.println("**************************************************");
 		cx3.desconectar();
 		System.out.println("**************************************************");
-
-		iconexion cx4 = Fabrica.getConexion("MysQL");
+		
+		iconexion cx4 = Fabrica.getConexion("Oracle");
 		System.out.println();
-		System.out.println("4.	Conexion  con MySQL Server");
+		System.out.println("5.	Conexion con Oracle");
 		System.out.println();
 		System.out.println("**************************************************");
 		cx4.conectar();
 		System.out.println("**************************************************");
 		cx4.desconectar();
-		System.out.println("**************************************************");
-		
-		iconexion cx5 = Fabrica.getConexion("Oracle");
-		System.out.println();
-		System.out.println("5.	Conexion con Oracle");
-		System.out.println();
-		System.out.println("**************************************************");
-		cx5.conectar();
-		System.out.println("**************************************************");
-		cx5.desconectar();
 		System.out.println("**************************************************");
 	}
 
